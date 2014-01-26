40c3c7e52845c90e13c49129e47a8ce743b7da8c
Pedro Larroy <pedro.larroy@gmail.com>
2014-01-26 04:21:49 +0100

    protocol.hpp and rename message.h to message.hpp

---
+++ src/cs/protocol.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+
+/*
+ *
+ *
+ * State transition table
+ *
+ *
+ *
+ *
+ *
+ */
+
+namespace cs
+{
+
+namespace protocol
+{
+
+enum class State
+{
+    INVALID = 0
+
+    INITIAL,
+};
+
+/**
+ * @brief protocol state
+ * @author plarroy
+ */
+class ProtocolState 
+{
+
+
+
+};
+
+} // end ns
+} // end ns
