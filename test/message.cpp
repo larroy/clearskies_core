776883f1a0de6b73b3c870fd399750b8040b4641
Daniel Cachapa <cachapa@gmail.com>
2014-02-03 10:25:21 +0100

    Add message tests

--- test/message.cpp
+++ test/message.cpp
@@ -21,7 +21,7 @@
 using namespace std;
 using namespace cs::message;
 
-BOOST_AUTO_TEST_CASE(MessageTest_type) {
+BOOST_AUTO_TEST_CASE(MessageTest_type_from_str) {
     BOOST_CHECK(mtype_from_string("unknown") == MType::UNKNOWN);
     BOOST_CHECK(mtype_from_string("__internal_start") == MType::INTERNAL_START);
     BOOST_CHECK(mtype_from_string("ping") == MType::PING);
@@ -39,4 +39,23 @@ BOOST_AUTO_TEST_CASE(MessageTest_type) {
     BOOST_CHECK(mtype_from_string("update") == MType::UPDATE);
     BOOST_CHECK(mtype_from_string("move") == MType::MOVE);
     BOOST_CHECK(mtype_from_string("aarsrasrasa") == MType::UNKNOWN);
+}
+
+BOOST_AUTO_TEST_CASE(MessageTest_type_to_str) {
+    BOOST_CHECK(mtype_to_string(MType::UNKNOWN) == "unknown");
+    BOOST_CHECK(mtype_to_string(MType::INTERNAL_START) == "__internal_start");
+    BOOST_CHECK(mtype_to_string(MType::PING) == "ping");
+    BOOST_CHECK(mtype_to_string(MType::GREETING) == "greeting");
+    BOOST_CHECK(mtype_to_string(MType::START) == "start");
+    BOOST_CHECK(mtype_to_string(MType::CANNOT_START) == "cannot_start");
+    BOOST_CHECK(mtype_to_string(MType::STARTTLS) == "starttls");
+    BOOST_CHECK(mtype_to_string(MType::IDENTITY) == "identity");
+    BOOST_CHECK(mtype_to_string(MType::KEYS) == "keys");
+    BOOST_CHECK(mtype_to_string(MType::KEYS_ACKNOWLEDGMENT) == "keys_acknowledgment");
+    BOOST_CHECK(mtype_to_string(MType::MANIFEST) == "manifest");
+    BOOST_CHECK(mtype_to_string(MType::GET_MANIFEST) == "get_manifest");
+    BOOST_CHECK(mtype_to_string(MType::GET) == "get");
+    BOOST_CHECK(mtype_to_string(MType::FILE_DATA) == "file_data");
+    BOOST_CHECK(mtype_to_string(MType::UPDATE) == "update");
+    BOOST_CHECK(mtype_to_string(MType::MOVE) == "move");
 }
