94e0233646a9790294208fc1134a800ed41de7ab
Pedro Larroy <pedro.larroy@gmail.com>
2014-02-04 20:56:19 +0100

    file data structure

---
+++ src/cs/scanner.hpp
@@ -0,0 +1,33 @@
+/*
+ *  This file is part of clearskies_core file synchronization program
+ *  Copyright (C) 2014 Pedro Larroy
+
+ *  clearskies_core is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+
+ *  clearskies_core is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with clearskies_core.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+#include "int_types.h"
+#include "utils.hpp"
+#include "file.hpp"
+
+namespace cs
+{
+namespace scanner
+{
+
+
+
+
+} // end ns
+} // end ns
