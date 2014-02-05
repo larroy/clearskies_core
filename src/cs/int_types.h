7728321225c2dbeac24313ac451128b5a7fbc39b
Dmitry Yakimenko <detunized@gmail.com>
2014-02-05 13:22:16 +0100

    Typo corrected

--- src/cs/int_types.h
+++ src/cs/int_types.h
@@ -3,7 +3,7 @@
 
 typedef int8_t i8;
 typedef uint8_t u8;
-typedef int16_t i6;
+typedef int16_t i16;
 typedef uint16_t u16;
 typedef int32_t i32;
 typedef uint32_t u32;
@@ -12,7 +12,7 @@ typedef uint64_t u64;
 
 typedef int8_t int8;
 typedef uint8_t uint8;
-typedef int16_t int6;
+typedef int16_t int16;
 typedef uint16_t uint16;
 typedef int32_t int32;
 typedef uint32_t uint32;
