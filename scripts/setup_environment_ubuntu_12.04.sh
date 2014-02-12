1287e4da084a3e69ecfbbd907ddcac3534dff12a
Pedro Larroy <pedro.larroy@gmail.com>
2014-02-12 11:56:25 +0100

    [ci] cleanup

---
+++ scripts/setup_environment_ubuntu_12.04.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# GYP (conflicts with the installed version of gyp)
+svn export -q http://gyp.googlecode.com/svn/trunk/ -r 1845 gyp
+# ninja
+wget https://github.com/martine/ninja/releases/download/v1.4.0/ninja-linux.zip
+unzip -d /usr/local/bin/ ninja-linux.zip
+# boost, g++-4.8, sqlite3
+add-apt-repository ppa:ubuntu-toolchain-r/test -y
+add-apt-repository ppa:boost-latest/ppa -y
+apt-get update -qq
+apt-get install libboost-filesystem1.55-dev libboost-test1.55-dev
+apt-get install libsqlite3-dev
+apt-get install -qq g++-4.8
