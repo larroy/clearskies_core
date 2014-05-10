81724bbc8e35d5f10ecd4701cdc99db32148da02
Pedro Larroy <pedro.larroy@gmail.com>
2014-05-10 14:02:13 +0200

    Unit test fixes

--- test/protocolstate.cpp
+++ test/protocolstate.cpp
@@ -110,7 +110,7 @@ BOOST_AUTO_TEST_CASE(find_messsage_test_03)
 
 BOOST_AUTO_TEST_CASE(find_messsage_test_04)
 {
-    string buff = "!7:{jsonz}\n";
+    string buff("!\x00\x00\x00\x07:{jsonz}", 13);
     MsgRstate mrs = find_message(buff);
     BOOST_CHECK(mrs.found);
     BOOST_CHECK(! mrs.garbage);
@@ -118,13 +118,12 @@ BOOST_AUTO_TEST_CASE(find_messsage_test_04)
     BOOST_CHECK_EQUAL(mrs.prefix, '!');
     BOOST_CHECK_EQUAL(string(mrs.encoded, mrs.encoded_sz), "{jsonz}");
     BOOST_CHECK_EQUAL(mrs.encoded_sz, 7u);
-    BOOST_CHECK_EQUAL(mrs.end, 11);
+    BOOST_CHECK_EQUAL(mrs.end, 13);
 }
 
 BOOST_AUTO_TEST_CASE(find_messsage_test_05)
 {
-    string buff = "!7:{jsonz}\n5\npayld";
-    //             01234 5 6
+    string buff("!\x00\x00\x00\x07:{jsonz}\x00\x00\x00\x05:payld", 23);
     MsgRstate mrs = find_message(buff);
     BOOST_CHECK(mrs.found);
     BOOST_CHECK(! mrs.garbage);
@@ -137,8 +136,7 @@ BOOST_AUTO_TEST_CASE(find_messsage_test_05)
 
 BOOST_AUTO_TEST_CASE(find_messsage_test_06)
 {
-    string buff = "s7:{jsonz}\nsignz\n";
-    //             01234 5 6
+    string buff("s\x00\x00\x00\x07:{jsonz}\x00\x00\x00\x05:signz", 23);
     MsgRstate mrs = find_message(buff);
     BOOST_CHECK(mrs.found);
     BOOST_CHECK(! mrs.garbage);
